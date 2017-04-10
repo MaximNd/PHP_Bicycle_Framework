f455a4b4839cb281baf2c40676f362a47644a2b7
Max <max0350@gmail.com>
2017-04-10 23:21:35 +0300

    Class Router and testing with conrollers

---
+++ app/controllers/PostsNew.php
@@ -0,0 +1,25 @@
+<?php
+
+class PostsNew {
+
+//    public function __construct() {
+//        echo 'PostsNew::__construct()';
+//    }
+
+    public function indexAction() {
+        echo 'PostsNew::index()';
+    }
+
+    public function testAction() {
+        echo 'PostsNew::test()';
+    }
+
+    public function testPageAction() {
+        echo 'PostsNew::testPage()';
+    }
+
+    public function before() {
+        echo 'PostsNew::before()';
+    }
+
+}
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -8,23 +8,69 @@ final class Router {
     public static function add($regexp, $route = []) {
         self::$routes[$regexp] = $route;
     }
-    
+
     public static function getRoutes() {
         return self::$routes;
     }
-    
+
     public static function getRoute() {
         return self::$route;
     }
-    
-    public static function matchRoute($url) {
+
+    private static function matchRoute($url) {
         foreach (self::$routes as $pattern => $route) {
-            if($url == $pattern) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                foreach ($matches as $key => $value) {
+                    if (is_string($key)) {
+                        $route[$key] = $value;
+                    }
+                }
+                if (!isset($route['action'])) {
+                    $route['action'] = 'index';
+                }
                 self::$route = $route;
                 return true;
             }
         }
         return false;
     }
-    
+
+    /**
+     * перенапрвляет URL по корректному маршруту
+     * @param string $url входящий URL
+     * @return void
+     */
+    public static function dispatch($url) {
+        if (self::matchRoute($url)) {
+            $controller = self::upperCamelCase(self::$route['controller']);
+
+            //self::upperCamelCase($controller);
+            if (class_exists($controller)) {
+                $controllerObj = new $controller;
+                $action = self::lowerCamelCase(self::$route['action']) . "Action";
+                if (method_exists($controllerObj, $action)) {
+                    $controllerObj->$action();
+                } else {
+                    echo "Method $controller::$action NOT FOUND!!!!";
+                }
+            } else {
+                echo "Conroller $controller NOT FOUND!!!!";
+            }
+        } else {
+            http_response_code(404);
+            include '404.html';
+        }
+    }
+
+    protected static function upperCamelCase($name) {
+        $name = str_replace('-', ' ', $name);
+        $name = ucwords($name);
+        $name = str_replace(' ', '', $name);
+        return $name;
+    }
+
+    protected static function lowerCamelCase($name) {
+        return lcfirst(self::upperCamelCase($name));
+    }
+
 }
